6064ffb8de48f4ad10f59b531e6e1689652b07f4
Vivekanand Trivedi <vivekanandtrivedi@Vivekanands-MacBook-Air.local>
2026-07-08 00:42:35 +0530

    exception handling

--- response-model.py
+++ response-model.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from os import name
+
+from fastapi import FastAPI, status, HTTPException,Request
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from starlette.requests import Request
 
 app = FastAPI()
 
@@ -19,3 +23,35 @@ def user():
         "email": "harsh@123",
         "password": 12345
     }
+@app.post("/user", status_code= status.HTTP_201_CREATED)
+def create_user():
+    return{"msg" : "user created"}
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    if user_id != 1:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND)
+    return{
+        "name": "sumit",
+        "email": "harsh@123",
+    }
+
+class UserNotFound(Exception):
+    def __init__(self, cos_id: int):
+        self.cos_id = cos_id
+
+@app.exception_handler(UserNotFound)
+def user_not_found(request: Request, exc: UserNotFound):
+    return JSONResponse(
+        status_code= 400,
+        content={"message": exc.cos_id}
+    )
+
+@app.get("/user/cos/{cos_id}")
+def get_user_cos_id(cos_id: int):
+    if cos_id != 1:
+        raise UserNotFound(cos_id)
+    return{
+        "name" : cos_id
+    }
+
